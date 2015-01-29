c3b2cf4eede26c77fba2d9fb0c2f7f28908732f4
Kristina Schneider <me@kristinaschneider.com>
2015-01-28 18:59:18 -0800

    improve FOUT; add CTA to navi

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
             ],
             tasks: ['compass:dev'],
             options: {
-                atBegin: true
+              atBegin: true
             }
           },
           autoprefix: {
